4e6b1072d60f9ccbcab66d1868aeaea985bf594a
Jaime-Gabriel-Hernandez-Garcia <jhernandezg1915@alumno.ipn.mx>
2021-12-10 11:14:34 -0600

    Practicas Java

---
+++ JavaPractica17/src/javapractica17/JavaPractica17.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractica17;
+
+/**
+ *
+ * @author ACER01
+ */
+public class JavaPractica17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
